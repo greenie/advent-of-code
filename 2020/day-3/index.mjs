932c54f94102d4b4ec98ebfc7c967e435f063ec3
Joe Green <greenie@users.noreply.github.com>
2020-12-03 20:52:41 +0000

    Day 3, part 2

--- 2020/day-3/index.mjs
+++ 2020/day-3/index.mjs
@@ -3,13 +3,19 @@ import fs from 'fs'
 const courseMap = fs.readFileSync('./input').toString().split('\n')
 
 const TREE = '#'
-const DX = 3
-const DY = 1
 
-const treesEncountered = (x, y, encountered) => {
+const movements = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2]
+]
+
+const treesEncountered = (x, y, dx, dy, encountered) => {
   const line = courseMap[y]
 
-  if (!line.length) {
+  if (!line || !line.length) {
     return encountered
   }
 
@@ -19,7 +25,16 @@ const treesEncountered = (x, y, encountered) => {
     encountered += 1
   }
 
-  return treesEncountered(x + DX, y + DY, encountered)
+  return treesEncountered(x + dx, y + dy, dx, dy, encountered)
 }
 
-console.log(treesEncountered(0, 0, 0))
+const treesProduct = movements.map(([dx, dy]) =>
+  treesEncountered(0, 0, dx, dy, 0)
+).reduce((a, b) => a * b)
+
+// part 1
+console.log(treesEncountered(0, 0, 3, 1, 0))
+
+// part 2
+console.log(treesProduct)
+
