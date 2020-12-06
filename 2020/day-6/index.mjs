49e84cd36bc72ec1eb35ba608d6c2a58f4af3fd3
Joe Green <greenie@users.noreply.github.com>
2020-12-06 14:34:28 +0000

    Day 6, part 2

--- 2020/day-6/index.mjs
+++ 2020/day-6/index.mjs
@@ -1,15 +1,49 @@
 import fs from 'fs'
 
-const allGroupAnswers = fs.readFileSync('./input').toString().split('\n\n')
+const groups = fs.readFileSync('./input').toString().split('\n\n').filter(g => g)
 
 const getUniqueAnswers = groupAnswer => {
   const answers = groupAnswer.replace(/\n/gm, '').split('')
   return new Set(answers)
 }
 
-const allAnswersSum = allGroupAnswers.reduce((count, answers) => {
+const getAnswers = groupAnswer => {
+  const answers = groupAnswer
+    .split('\n')
+    .filter(a => a)
+    .map(a => a.split(''))
+
+  return answers
+}
+
+const intersection = (a, b) => {
+  const setA = new Set(a)
+  const setB = new Set(b)
+  return [...setA].filter(x => setB.has(x))
+}
+
+const allAnswersSum = groups.reduce((count, answers) => {
   const unique = getUniqueAnswers(answers)
   return count += unique.size
 }, 0)
 
+const allIntersectingAnswersSum = groups.reduce((count, group) => {
+  const answers = getAnswers(group)
+  let commonAnswers = answers[0]
+
+  for (let i = 1; i < answers.length; i++) {
+    commonAnswers = intersection(commonAnswers, answers[i])
+
+    if (commonAnswers.length === 0) {
+      break
+    }
+  }
+
+  return count += commonAnswers.length
+}, 0)
+
+// part 1
 console.log(allAnswersSum)
+
+// part 2
+console.log(allIntersectingAnswersSum)
