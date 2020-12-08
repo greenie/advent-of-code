3f72bc2ad7a185385be1e52d8b1a8d53dc175503
Joe Green <greenie@users.noreply.github.com>
2020-12-08 11:27:04 +0000

    Day 8, part 2

--- 2020/day-8/index.mjs
+++ 2020/day-8/index.mjs
@@ -5,7 +5,38 @@ const instructions = fs.readFileSync('./input').toString().split('\n').filter(c 
 const parseInstruction = instruction => {
   const [op, arg] = instruction.split(' ')
 
-  return { op, arg: Number(arg), hasRun: false }
+  return { op, arg: Number(arg) }
+}
+
+const run = (program, index, acc, stack) => {
+  if (index === program.length) {
+    return {
+      exit: 0,
+      acc,
+      stack
+    }
+  }
+
+  if (!program[index] || stack.includes(index)) {
+    return {
+      exit: 1,
+      acc,
+      stack
+    }
+  } else {
+    stack.push(index)
+  }
+
+  const { op, arg } = program[index]
+
+  switch(op) {
+    case 'acc':
+      return run(program, index + 1, acc + arg, stack)
+    case 'jmp':
+      return run(program, index + arg, acc, stack)
+    case 'nop':
+      return run(program, index + 1, acc, stack)
+  }
 }
 
 const program = []
@@ -14,24 +45,29 @@ instructions.forEach(instruction => {
   program.push(parseInstruction(instruction))
 })
 
-const run = (program, index, acc) => {
-  const { op, arg, hasRun } = program[index]
-
-  if (hasRun) {
-    return acc
-  } else {
-    program[index].hasRun = true
-  }
-
-  switch(op) {
-    case 'acc':
-      return run(program, index + 1, acc + arg)
-    case 'jmp':
-      return run(program, index + arg, acc)
-    case 'nop':
-      return run(program, index + 1, acc)
-  }
-}
+let result = run(program, 0, 0, [])
 
 // part 1
-console.log(run(program, 0, 0))
+console.log(result.acc)
+
+// part 2
+result.stack.reverse().forEach(progIndex => {
+  const newProgram = JSON.parse(JSON.stringify(program))
+  const { op } = newProgram[progIndex]
+
+  switch (op) {
+    case 'jmp':
+      newProgram.splice(progIndex, 1, { ...newProgram[progIndex], op: 'nop' })
+      break
+    case 'nop':
+      newProgram.splice(progIndex, 1, { ...newProgram[progIndex], op: 'jmp' })
+  }
+
+  const newResult = run(newProgram, 0, 0, [])
+
+  if (newResult.exit === 0) {
+    result = newResult
+  }
+})
+
+console.log(result.acc)
