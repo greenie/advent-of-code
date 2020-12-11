58428aea4f8d8af660efc6042bdccebe895a3e6e
Joe Green <greenie@users.noreply.github.com>
2020-12-11 13:24:30 +0000

    Day 9, parts 1 and 2

---
+++ 2020/day-9/index.mjs
@@ -0,0 +1,58 @@
+import { match } from 'assert'
+import fs from 'fs'
+import {add} from '../../util.mjs'
+
+const input = fs.readFileSync('./input').toString().split('\n').filter(n => n).map(Number)
+
+const findInvalid = (numbers, preamble) => {
+  let invalidNumber
+
+  for (let i = preamble; i < numbers.length; i++) {
+    let valid = false
+
+    for (let j = i - preamble; j < i; j++) {
+      for (let k = i - preamble; k < i; k++) {
+        if (!valid && input[j] + input[k] === input[i]) {
+          valid = true
+        }
+      }
+    }
+
+    if (!valid) {
+      invalidNumber = input[i]
+      break
+    }
+  }
+
+  return invalidNumber
+}
+
+const findRange = (numbers, target) => {
+  let i = 0
+  const contiguous = []
+
+  while (i < numbers.length) {
+    const num = numbers[i]
+    const sum = [...contiguous, num].reduce(add)
+
+    if (sum === target) {
+      contiguous.push(num)
+      return Math.min(...contiguous) + Math.max(...contiguous)
+    }
+
+    if (sum < target) {
+      contiguous.push(num)
+      i += 1
+    } else {
+      contiguous.shift()
+    }
+  }
+}
+
+const invalidNumber = findInvalid(input, 25)
+
+// part 1
+console.log(invalidNumber)
+
+// part 2
+console.log(findRange(input, invalidNumber))
