4d404ed2667e7abeba0eaec1a582b6aa3422566f
Joe Green <greenie@users.noreply.github.com>
2020-12-11 13:26:24 +0000

    Day 10, parts 1 and 2

---
+++ 2020/day-10/index.mjs
@@ -0,0 +1,40 @@
+import fs from 'fs'
+import {add, multiply} from '../../util.mjs'
+
+const adapters = fs.readFileSync('./input')
+  .toString()
+  .split('\n')
+  .filter(n => n)
+  .map(Number)
+  .sort((a, b) => a - b)
+
+const countDiffs = (list, ...targets) => list.reduce((acc, item, i, arr) => {
+  if (!arr[i + 1]) {
+    return acc
+  }
+
+  const diff = arr[i + 1] - item
+
+  if (targets.includes(diff)) {
+    acc[diff] = acc[diff] ? acc[diff] + 1 : 1
+  }
+
+  return acc
+}, {})
+
+const validArrangements = (list, ...allowedDiffs) => list.reduce((acc, adapter) => {
+  acc[adapter] = allowedDiffs
+    .map(diff => acc[adapter - diff] || 0)
+    .reduce(add)
+
+  return acc
+}, [1])
+
+const deviceAdapter = adapters[adapters.length - 1] + 3
+const joltages = [0, ...adapters, deviceAdapter]
+
+// part 1
+console.log(Object.values(countDiffs(joltages, 1, 3)).reduce(multiply))
+
+// part 2
+console.log(validArrangements(adapters, 1, 2, 3).pop())
--- util.mjs
+++ util.mjs
@@ -1,1 +1,2 @@
 export const add = (x, y) => x + y
+export const multiply = (x, y) => x * y
