6ed5968f88aae389c851a703ea9a919ade4353d6
Joe Green <greenie@users.noreply.github.com>
2020-12-18 22:45:48 +0000

    Day 13, part 2

--- 2020/day-13/index.mjs
+++ 2020/day-13/index.mjs
@@ -1,16 +1,60 @@
 import fs from 'fs'
-import { sortAsc } from '../../util.mjs'
+import { multiply, sortAsc } from '../../util.mjs'
 
 const input = fs.readFileSync('./input').toString().split('\n').filter(n => n)
 
 const departureTime = parseInt(input[0])
-const buses = input[1].split(',').filter(id => id !== 'x').map(Number)
+const buses = input[1].split(',')
 
-const applicableBuses = buses
-  .map(bus => ({ id: bus, earliestTime: bus * Math.ceil(departureTime / bus) }))
-  .filter(({ earliestTime }) => earliestTime >= departureTime)
+const modInv = (a, mod) => {
+  const b = a % mod
+
+  for (let i = 1n; i <= mod; i++) {
+    if ((b * i) % mod === 1n) {
+      return i
+    }
+  }
+
+  return 1n
+}
+
+const chineseRemainder = (numbers, remainders) => {
+  const product = numbers.reduce(multiply, 1n)
+
+  const sum = numbers.reduce((acc, num, i) => {
+    const p = product / num
+    return acc += remainders[i] * p * modInv(p, num)
+  }, 0n)
+
+  return sum % product
+}
+
+const busesByDepartueTime = buses
+  .filter(id => id !== 'x')
+  .map(bus => {
+    const id = Number(bus)
+    return { id, earliestTime: id * Math.ceil(departureTime / id) }
+  })
   .sort((a, b) => sortAsc(a.earliestTime, b.earliestTime))
 
+const busesByPosition = buses
+  .map((id, i) => ({ i, id }))
+  .filter(({ id }) => id !== 'x')
+  .map(({ i, id }) => ({
+    number: BigInt(id),
+    i,
+    remainder: BigInt(id - i)
+  }))
+
 // part 1
-const earliest = applicableBuses[0]
+const earliest = busesByDepartueTime[0]
 console.log((earliest.earliestTime - departureTime) * earliest.id)
+
+// part 2
+const [numbers, remainders] = busesByPosition.reduce((acc, bus) => {
+  acc[0].push(bus.number)
+  acc[1].push(bus.remainder)
+  return acc
+}, [[], []])
+
+console.log(chineseRemainder(numbers, remainders))
--- util.mjs
+++ util.mjs
@@ -2,4 +2,4 @@ export const add = (x, y) => x + y
 export const multiply = (x, y) => x * y
 export const distance = (x, y) => Math.abs(x) + Math.abs(y)
 export const sortAsc = (a, b) => a - b
-export const sortDesc = (a, b) => a + b
+export const sortDesc = (a, b) => b - a
