852959e78ac9495987be7c6c4a7fab2235178630
Joe Green <greenie@users.noreply.github.com>
2020-12-21 12:38:48 +0000

    Day 14, part 2

--- 2020/day-14/index.mjs
+++ 2020/day-14/index.mjs
@@ -3,13 +3,29 @@ import { add } from '../../util.mjs'
 
 const input = fs.readFileSync('./input').toString().split('\n').filter(n => n)
 
-const toBinaryString = number => number.toString(2).padStart(36, '0')
+const toBinaryString = (number, length = 36) => number.toString(2).padStart(length, '0')
+
+const combinations = n => {
+  const max = 2**n
+  const result = []
+
+  for (let i = 0; i < max; i++) {
+    result.push(toBinaryString(i, n))
+  }
+
+  return result
+}
+
+const parseMask = mask => ({
+  raw: mask,
+  combinations: combinations(mask.match(/X/g).length)
+})
 
 const parseInput = input => input.reduce((acc, line) => {
   const i = acc.length - 1
 
   if (/^mask/.test(line)) {
-    acc.push({ mask: line.slice(7), commands: [] })
+    acc.push({ mask: parseMask(line.slice(7)), commands: [] })
   } else {
     const { groups: { address, value } }= /^mem\[(?<address>\d+)\] = (?<value>\d+)$/g.exec(line)
     acc[i].commands.push([parseInt(address), parseInt(value)])
@@ -19,27 +35,60 @@ const parseInput = input => input.reduce((acc, line) => {
 }, [])
 
 const program = parseInput(input)
-const memory = new Map()
 
-program.forEach(({ mask, commands }) => {
-  commands.forEach(([address, value]) => {
-    const result = toBinaryString(value).split('').map((c, i) => {
-      switch (mask.charAt(i)) {
-        case '1':
-          return '1'
+const applyMaskToValues = program => {
+  const memory = new Map()
 
-        case '0':
-          return '0'
+  program.forEach(({ mask, commands }) => {
+    commands.forEach(([address, value]) => {
+      const result = toBinaryString(value).split('').map((c, i) => {
+        switch (mask.raw.charAt(i)) {
+          case '1':
+            return '1'
 
-        case 'X':
-        default:
-          return c
-      }
+          case '0':
+            return '0'
+
+          case 'X':
+          default:
+            return c
+        }
+      })
+
+      memory.set(address, parseInt(result.join(''), 2))
     })
-
-    memory.set(address, parseInt(result.join(''), 2))
   })
-})
+
+  return [...memory.values()].reduce(add)
+}
+
+const applyMaskToAddresses = program => {
+  const memory = new Map()
+
+  program.forEach(({ mask, commands }) => {
+    commands.forEach(([address, value]) => {
+      const binaryAddress = toBinaryString(address)
+
+      mask.combinations.forEach(combination => {
+        let xPos = 0
+        const address = mask.raw.split('').map((c, i) => {
+          if (c === 'X') {
+            return combination[xPos++]
+          }
+
+          return parseInt(c) | parseInt(binaryAddress[i])
+        }).join('')
+
+        memory.set(address, value)
+      })
+    })
+  })
+
+  return [...memory.values()].reduce(add)
+}
 
 // part 1
-console.log([...memory.values()].reduce(add))
+console.log(applyMaskToValues(program))
+
+// part 2
+console.log(applyMaskToAddresses(program))
