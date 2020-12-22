cdcf0740aec44ff9132cd2de99382274b847bcbb
Joe Green <greenie@users.noreply.github.com>
2020-12-22 20:56:41 +0000

    Day 16, part 2

--- 2020/day-16/index.mjs
+++ 2020/day-16/index.mjs
@@ -1,31 +1,81 @@
 import fs from 'fs'
-import { add, range } from '../../util.mjs'
+import { add, multiply, range } from '../../util.mjs'
 
-const input = fs.readFileSync('./input').toString().split('\n\n').filter(l => l)
+const input = fs.readFileSync('./input', 'utf-8').trim().split('\n\n')
 
-const ranges = new Set(input[0].split('\n').flatMap(line => {
+const fields = input[0].split('\n').map(line => {
   const [lower, upper] = [...line.matchAll(/\d+-\d+/g)].map(match => match[0])
 
   const lowerRange = range(...lower.split('-').map(Number))
   const upperRange = range(...upper.split('-').map(Number))
 
-  return [...lowerRange.concat(upperRange)]
-}))
+  return lowerRange.concat(upperRange)
+})
+
+const myTicket = input[1]
+  .split('\n')
+  .slice(1)
+  .flatMap(ticket => ticket.split(',').map(Number))
 
 const nearbyTickets = input[2]
   .split('\n')
   .slice(1)
   .map(ticket => ticket.split(',').map(Number))
 
-const invalidTicketValues = nearbyTickets.reduce((acc, ticket) => {
-  ticket.forEach(value => {
-    if (!ranges.has(value)) {
-      acc.push(value)
-    }
-  })
+const fullRange = new Set(fields.flat())
+
+const [invalidValues, validTickets] = nearbyTickets.reduce((acc, ticket) => {
+  const invalidValue = ticket.find(value => !fullRange.has(value))
+
+  if (invalidValue !== undefined) {
+    acc[0].push(invalidValue)
+  } else {
+    acc[1].push(ticket)
+  }
 
   return acc
-}, [])
+}, [[], []])
+
+const getFieldOrder = (tickets, fields) => {
+  const ticketIndices = tickets[0].map((_, i) => i)
+  const fieldOrder = []
+
+  let matches = fields.map((field, fi) => {
+    const indices = ticketIndices.filter(ti =>
+      tickets.every(ticket => field.some(value => value === ticket[ti]))
+    )
+
+    return [fi, indices]
+  })
+
+  while (matches.length) {
+    let [found, notFound] = matches.reduce((acc, match) => {
+      if (match[1].length === 1) {
+        acc[0].push(match)
+      } else {
+        acc[1].push(match)
+      }
+
+      return acc
+    }, [[], []])
+
+    matches = notFound
+
+    found.forEach(([i, col]) => {
+      fieldOrder[i] = col[0]
+      matches = matches.map(match => {
+        match[1] = match[1].filter(value => value !== col[0])
+        return match
+      })
+    })
+  }
+
+  return fieldOrder
+}
 
 // part 1
-console.log(invalidTicketValues.reduce(add))
+console.log(invalidValues.reduce(add))
+
+// part 2
+const order = getFieldOrder(validTickets, fields)
+console.log(range(0, 5).map(n => order[n]).map(i => myTicket[i]).reduce(multiply))
